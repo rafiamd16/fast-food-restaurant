cee0fd4f4adc90da46879d273fb95a9edcc242d6
RafiAmd16 <rafiahmad05111@gmail.com>
2024-03-09 23:31:55 +0800

    rubah dikit

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,8 +16,8 @@ window.onscroll = () => {
 };
 
 // Hamburger
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
